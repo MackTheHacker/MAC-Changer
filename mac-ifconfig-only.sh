c9a6ad6c84ba7c8e8a7fdef6c7c8b7e767d19d74
MackTheHacker <47538637+MackTheHacker@users.noreply.github.com>
2019-03-05 16:34:57 +0000

    Update mac-ifconfig-only.sh

--- mac-ifconfig-only.sh
+++ mac-ifconfig-only.sh
@@ -2,7 +2,7 @@
 # A simple shell script that changes your MAC address. 
 # The name of the device must be provided as a command line argument in order to change its MAC address (you can use ifconfig to see your devices) 
 # Also, this script will only work if run as root. It will fail if not done so.  
-# By default, it sets your MAC address to 00:11:22:33:44:55. You can change this to whatever value you want. If you want a randomly assigned one, then use the other shell script. 
+# By default, it sets your MAC address to 00:11:22:33:44:55. You can change this to whatever value you want. 
 
 if [ -z "$1" ]; then 
 	(>&2 printf "ERROR: You must provide the name of the device in order to change its MAC address.\nGet your device name by using \"ifconfig\"\n")
